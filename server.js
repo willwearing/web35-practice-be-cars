c691cc8761931525f56f4b87f53ac03fd7b15c61
Zac Smith <zrs3141592@gmail.com>
2020-09-16 13:05:37 -0600

    basic app scaffold

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const colors = require('colors')
+const cors = require('cors')
+
+const connectDB = require('./utils/db.js')
+require('dotenv').config()
 
 const server = express()
 
+server.use(cors())
+server.use(helmet())
+server.use(morgan('dev'))
 server.use(express.json())
+connectDB()
 
 let currentTime = new Date().toLocaleString()
 
@@ -18,5 +29,5 @@ server.get('/', (req, res) => {
 const PORT = process.env.PORT || 3555
 
 server.listen(PORT, () => {
-  console.log(`\n** Server is listening on port ${PORT} **\n`)
+  console.log(`\n** Server is listening on port ${PORT} **\n`.rainbow)
 })
