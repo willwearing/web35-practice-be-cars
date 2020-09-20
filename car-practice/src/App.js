78d824d2c0e188b87be71c00af1db5cee6fbfe9e
willwearing <willwearing@gmail.com>
2020-09-20 14:28:36 -0600

    axios car data is being displayed correctly and have added basic border to show the breakpoints between cards

--- car-practice/src/App.js
+++ car-practice/src/App.js
@@ -12,14 +12,19 @@ function App() {
   const [cars, setCars] = useState([]);
 
   const getData = () => {
+    setIsFetchingData(true);
+
     axios
       .get("https://cars-be-practice.herokuapp.com/api/cars/")
       .then((response) => {
         setCars(response.data);
+        // console.log(cars); - response.data is being correctly set in state to cars
+        setIsFetchingData(false);
       })
       .catch((error) => {
         console.log(`Axios error: ${error}`);
         setError(error);
+        setIsFetchingData(false);
       });
   };
 
@@ -27,7 +32,12 @@ function App() {
     <div className="App">
       <h1>Car Database and Car Data Input Form</h1>
       <CarForm />
-      <CarList getData={getData} />
+      <CarList
+        getData={getData}
+        error={error}
+        cars={cars}
+        isFetchingData={isFetchingData}
+      />
     </div>
   );
 }
